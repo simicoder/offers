05fb65f0c1f1960b64477dc5834caace3aa2fb8f
szymon <szymon.popielarz@gmail.com>
2021-09-13 19:08:22 +0200

    added cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -7,12 +7,23 @@ import cookieParser from 'cookie-parser';
 import { notFound, catchErrors } from './middlewares/errors';
 import users from './routes/users';
 import offers from './routes/offers';
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const origin =
+  process.env.NODE_ENV === 'production' ? process.env.CLIENT_URL : 'http://localhost:3000';
+
+app.use(
+  cors({
+    credentials: true,
+    origin,
+  }),
+);
 
 app.use('/users', users);
 app.use('/offers', offers);
