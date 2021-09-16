20f305887422380f7195a011141e15e4e51064dc
szymon <szymon.popielarz@gmail.com>
2021-09-16 22:12:22 +0200

    chosen colour change

--- packages/client/components/atoms/MenuButton/MenuButton.tsx
+++ packages/client/components/atoms/MenuButton/MenuButton.tsx
@@ -17,7 +17,7 @@ export const MenuButton = memo<MenuButton>(({ name, href, current }) => {
       <a
         key={name}
         className={classNames(
-          current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+          current ? 'bg-chosen text-white' : 'text-gray-300 hover:bg-hover hover:text-white',
           'block px-3 py-2 rounded-md text-base font-medium',
         )}
         aria-current={current ? 'page' : undefined}
--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      main: '#242424',
+      hover: '#313131',
+      chosen: '#181818',
+      danger: '#e3342f',
+    }),
   },
   variants: {
     extend: {},
